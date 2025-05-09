5d1b8c5ae5220f2e119126bb446d203ccbda9cca
orlifera <oferazzani125@gmail.com>
2025-05-09 12:47:16 +0200

    fix deploy

--- src/app/filosofi/page.tsx
+++ src/app/filosofi/page.tsx
@@ -1,11 +1,9 @@
 
 import React from 'react'
-import BC from '@/components/BC'
 
 function page() {
     return (
         <>
-            {/* <BC currentPage={"Filosofi"} /> */}
             <div className='w-full h-screen'>filosofi</div>
         </>
     )
--- src/app/lettori/page.tsx
+++ src/app/lettori/page.tsx
@@ -6,7 +6,6 @@ import BC from '@/components/BC'
 function page() {
     return (
         <>
-            {/* <BC currentPage={"Lettori"} /> */}
             <div className='w-full h-screen'>lettori</div>
         </>
 
